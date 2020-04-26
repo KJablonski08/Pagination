686f1c1af5e76098ac1ea3b2e6a8752c66c1fda1
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-04-26 15:01:41 -0400

    showPage function added and working

--- js/script.js
+++ js/script.js
@@ -18,28 +18,28 @@ FSJS project 2 - List Filter and Pagination
 ***/
 
 // variable to store the student list item elements in the student list
-
 const studentListItems = document.getElementsByClassName('student-item cf'); 
 
 //variable to store the number of items to show on each “page” - for this project - 10
 const itemsPerPage = 10;
 
+//`showPage` function to hide all of the items in the list except for the ten you want to show.
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+function showPage(list, page) {
+   let beginIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = page * itemsPerPage;
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+   //display any list item with an index that is greater than or equal to the start index variable and less than the end index variable
+   for (let i = 0; i < list.length; i++) {
+      if ( i < beginIndex || i > endIndex) {
+         list[i].style.display = 'none';
+      } else {
+         console.log(list[i]);
+      }
+   }
+}
 
+showPage(studentListItems, 1);
 
 
 
